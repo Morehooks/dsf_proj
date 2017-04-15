4456b233f3f5673c3f55ec5bfacb2881b28fae35
jake <jakelwatson86@gmail.com>
2017-04-15 20:22:24 +0100

    Added mounted() and tided up.

--- static/master.js
+++ static/master.js
@@ -1,77 +1,8 @@
 
-Vue.component('message-item', {
-	props: ['message'],
-	template: '<p>{{ message }}</p>'
+Vue.component('data-item', {
+	props: ['raw_data'],
+	template: '<li>{{ raw_data.section_seq }}</li>'
 })
-
-var app = new Vue({
-	el: '#app',
-	data: {
-		message : 'Hello!'
-	}
-})
-
-var app2 = new Vue({
-	el: '#app2',
-	data: {
-		message : 'Todays date is: ' + new Date()
-	}
-})
-
-var app3 = new Vue({
-	el: '#app3',
-	data: {
-		seen: true
-	}
-})
-
-var app4 = new Vue({
-	el: '#app4',
-	data:{
-		todos:[
-			{ text: 'Learn JavaScript'},
-			{ text: 'Learn Vue'},
-			{ text: 'Build something awesome'},
-			{ text: 'Type in console app4.todos.push("json object here") to add a new item'}
-		]
-	}
-})
-
-var app5 = new Vue({
-	el: '#app5',
-	data: {
-		message : 'Reversable Message!'
-	},
-	methods: {
-		reverseMessage: function(){
-			this.message = this.message.split('').reverse().join('')
-		}
-	}
-})
-
-var app6 = new Vue({
-	el: '#app6',
-	data: {
-		message : 'Hello Vue!'
-	}
-})
-
-Vue.component('todo-item', {
-	props: ['todo'],
-	template: '<li>{{ todo.text }}</li>'
-})
-
-var app7 = new Vue({
-	el: '#app7',
-	data: {
-		groceryList: [
-			{text: 'Vegatables'},
-			{text: 'Cheese'},
-			{text: 'Stuff'}
-		]
-	}
-})
-
 
 var app8 = new Vue({
     el: '#app8',
@@ -81,6 +12,7 @@ var app8 = new Vue({
 	methods: {
         get_raw_data: function () {
             var self = this;
+            //noinspection JSUnresolvedVariable axios library.
             axios.get('/survey')
                 .then(function (response) {
                     console.log(response.data[0]);
@@ -90,10 +22,13 @@ var app8 = new Vue({
                     console.log(error);
                 });
         }
+    },
+    mounted(){
+        //runs method on page load
+        this.get_raw_data()
     }
 })
 
-app8.get_raw_data()
 
 
 
