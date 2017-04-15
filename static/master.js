dc3fed12a29ee40e4f18ab0df8db83d4ba88b3d6
jake <jakelwatson86@gmail.com>
2017-04-15 15:05:34 +0100

    Added ajax method

--- static/master.js
+++ static/master.js
@@ -71,3 +71,30 @@ var app7 = new Vue({
 		]
 	}
 })
+
+
+var app8 = new Vue({
+    el: '#app8',
+	data : {
+		raw_data : []
+	},
+	methods: {
+        get_raw_data: function () {
+            var self = this;
+            axios.get('/survey')
+                .then(function (response) {
+                    console.log(response.data[0]);
+                    self.raw_data = response.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+    }
+})
+
+app8.get_raw_data()
+
+
+
+
