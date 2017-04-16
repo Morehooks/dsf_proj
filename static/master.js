97aa9735bed0d0a3545e5d10a536ab50fa02d023
jake <jakelwatson86@gmail.com>
2017-04-16 16:52:08 +0100

    Added some presentation stuff

--- static/master.js
+++ static/master.js
@@ -7,25 +7,27 @@ Vue.component('data-item', {
 var app8 = new Vue({
     el: '#app8',
 	data : {
-		raw_data : []
+		raw_data : [],
+        questions : []
 	},
 	methods: {
         get_raw_data: function () {
             var self = this;
             //noinspection JSUnresolvedVariable axios library.
-            axios.get('/survey')
+            axios.get('/survey?page_seq=100')
                 .then(function (response) {
-                    console.log(response.data[0]);
+                    console.log(response.data);
                     self.raw_data = response.data;
                 })
                 .catch(function (error) {
                     console.log(error);
                 });
+
         }
     },
     mounted(){
         //runs method on page load
-        this.get_raw_data()
+        this.get_raw_data();
     }
 })
 
