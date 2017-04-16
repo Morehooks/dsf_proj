252b356e4ee6f444d27f3a38b3aacb975c6f4f31
jake <jakelwatson86@gmail.com>
2017-04-16 15:12:02 +0100

    Add filters to the backed

--- survey/views.py
+++ survey/views.py
@@ -1,9 +1,17 @@
 from rest_framework import generics
 from .models import Question
 from .serializers import QuestionSerializer
+import django_filters
+
+
+class QuestionFilter(django_filters.FilterSet):
+    class Meta:
+        model = Question
+        fields = ['page_seq', 'question_seq']
 
 
 class QuestionList(generics.ListAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
-
+    filter_backends = (django_filters.rest_framework.DjangoFilterBackend,)
+    filter_fields = ('page_seq', 'question_seq')
