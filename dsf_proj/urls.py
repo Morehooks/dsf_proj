2ddbe7f7e90945e2a5aed5f6e16d9c99a17a7f24
jake <jakelwatson86@gmail.com>
2017-04-17 16:41:47 +0100

    Finally got core added

---
+++ dsf_proj/urls.py
@@ -0,0 +1,32 @@
+"""dsf_proj URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+
+from django.conf.urls import url
+from django.contrib import admin
+from django.views.generic import TemplateView
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from survey import views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^survey/$', views.QuestionList.as_view()),
+    url(r'^$', TemplateView.as_view(template_name="templates/index.html")),
+    url(r'^router/$', TemplateView.as_view(template_name="templates/router_test.html")),
+    url(r'^page/$', TemplateView.as_view(template_name="templates/page_test.html")),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
